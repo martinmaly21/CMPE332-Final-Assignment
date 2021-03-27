6a360b774e214e9aa2afd683046f14c43ac84e4e
Martin Maly <martinmaly66@hotmail.com>
2021-03-27 12:14:43 -0400

    updated ui

--- airline.php
+++ airline.php
@@ -7,30 +7,40 @@
   <meta charset="utf-8">
   <title>Airline Assignment</title>
   <!-- Custom stylesheet -->
-  <link rel="stylesheet" href="style/airline.css">
-  <link rel="stylesheet" href="style/side_bar.css">
+  <link rel="stylesheet" href="css/airline.css">
+  <link rel="stylesheet" href="css/side_bar.css">
 </head>
 
 <!-- Page content -->
 <body>
   <!-- Side navigation -->
-  <div class="sidenav">
-    <div class="image-holder">
-      <img src="img/paper_airplane.png" alt="Airplane Assignment" width="150" height="150"">
-    </div>
-    <a href="#">Home</a>
-    <a href="#">Search flights</a>
-    <a href="#">Create flight</a>
-    <a href="#">Update flight</a>
-    <a href="#">Get average flight seats</a>
-  </div>
+<div class="sidenav">
+    <a class="image-holder", href= "airline.php">
+      <img src="img/paper_airplane.png" alt="Airplane Assignment" width="150" height="150">
+    </a>
+    <a class="sidenav_element" href="airline.php">
+        <h1>Home</h1>
+    </a>
+    <a class="sidenav_element" href="search_flights.php">
+        <h1>Search Flights</h1>
+    </a>
+    <a class="sidenav_element" href="add_flight.php">
+        <h1>Create flight</h1>
+    </a>
+    <a class="sidenav_element" href="update_flight.php">
+        <h1>Update Flight</h1>
+    </a>
+    <a class="sidenav_element" href="average_seats.php">
+        <h1>Get Average Flight Seats</h1>
+    </a>
+</div>
 
   <div class="main_view">
     <?php include 'connectdb.php';?>
 
     <h1>Airline Assignment</h1>
     <h2>Available Flights</h2>
-    <table id="data">
+    <table id="airlinedata">
       <tr>
                 <th>Airline |</th>
                 <th>Flight Code |</th>
