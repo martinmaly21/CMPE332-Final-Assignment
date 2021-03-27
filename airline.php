33936d98d8c931df294b9771d5c5836480d9142c
Martin Maly <martinmaly66@hotmail.com>
2021-03-27 11:06:11 -0400

    fixed styling of side bar

--- airline.php
+++ airline.php
@@ -6,19 +6,21 @@
 <head>
   <meta charset="utf-8">
   <title>Airline Assignment</title>
+  <!-- Custom stylesheet -->
+  <link rel="stylesheet" href="css/side_bar.css">
 </head>
-
-<!-- Side navigation -->
-<div class="sidenav">
-  <a href="#">Home</a>
-  <a href="#">Search flights</a>
-  <a href="#">Create flight</a>
-  <a href="#">Update flight</a>
-  <a href="#">Get average flight seats</a>
-</div>
 
 <!-- Page content -->
 <body>
+  <!-- Side navigation -->
+  <div class="sidenav">
+    <a href="#">Home</a>
+    <a href="#">Search flights</a>
+    <a href="#">Create flight</a>
+    <a href="#">Update flight</a>
+    <a href="#">Get average flight seats</a>
+  </div>
+
   <?php include 'connectdb.php';?>
   <div class="main_view">
     <h1>Airline Assignment</h1>
